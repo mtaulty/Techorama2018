2acb14251730f1967009c032801bda9e684cea13
Mike Taulty <mtaulty@microsoft.com>
2018-05-08 12:44:20 +0100

    Added animals and made them appear on speech commands

---
+++ TRProject/Assets/Scripts/AnimalFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimalFactory : MonoBehaviour
+{
+    public GameObject lionPrefab;
+    public GameObject owlPrefab;
+    public GameObject pigPrefab;
+
+    public GameObject MakeObjectForType(string type)
+    {
+        GameObject gameObject = null;
+
+        switch (type)
+        {
+            case "lion":
+                gameObject = GameObject.Instantiate(this.lionPrefab);
+                break;
+            case "pig":
+                gameObject = GameObject.Instantiate(this.pigPrefab);
+                break;
+            case "owl":
+                gameObject = GameObject.Instantiate(this.owlPrefab);
+                break;
+            default:
+                break;
+        }
+        // Give the game object a unique name which reflects the type it is
+        gameObject.name = string.Format("{0}_{1}", type, Guid.NewGuid());
+
+        return (gameObject);
+    }
+}
---
+++ TRProject/Assets/Scripts/MainScript.cs
@@ -0,0 +1,17 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.InputModule.Utilities.Interactions;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public void CreateAnimal(string type)
+    {
+        var newAnimal = this.gameObject.GetComponent<AnimalFactory>().MakeObjectForType(type);
+
+        newAnimal.transform.position =
+            Camera.main.transform.position +
+            (Camera.main.transform.forward * 3.0f);
+
+        newAnimal.transform.forward = Camera.main.transform.forward;
+    }
+}
